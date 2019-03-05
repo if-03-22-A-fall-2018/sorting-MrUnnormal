2239c02e7a935d16f9ba15eb56511aa54ccdf1f8
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-03-05 11:07:23 +0100

    tests

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -11,18 +11,49 @@
  *-----------------------------------------------------------------------------
 */
 #include "sorting_algorithms.h"
+#include <stdlib.h>
 
 void 	init_random (int *array, unsigned long length)
 {
-
+  for(unsigned long i = 0; i < length; i++)
+  {
+    array[i] = random();
+  }
 }
 
 void 	bubble_sort (int *array, unsigned long length)
 {
-
+  int last_unsorted = length - 1;
+  bool elements_were_swapped = false;
+  do
+  {
+      elements_were_swapped = false;
+      for(int j = 0; j < last_unsorted; j++)
+      {
+        if(array[j] > array[j + 1])
+        {
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          elements_were_swapped = true;
+        }
+      }
+      last_unsorted--;
+  } while(elements_were_swapped);
 }
 
 void 	insertion_sort (int *array, unsigned long length)
 {
-  
+   int i, key, j;
+   for (i = 1; i < length; i++)
+   {
+       key = array[i];
+       j = i-1;
+       while (j >= 0 && array[j] > key)
+       {
+           array[j+1] = array[j];
+           j = j-1;
+       }
+       array[j+1] = key;
+   }
 }
--- stopwatch.cpp
+++ stopwatch.cpp
@@ -11,18 +11,27 @@
  *-----------------------------------------------------------------------------
 */
 #include "stopwatch.h"
+#include <time.h>
+
+clock_t end;
+clock_t start;
 
 void 	start_stopwatch ()
 {
-
+  start = clock();
+  end = 0;
 }
 
 void 	stop_stopwatch ()
 {
-
+  end = clock();
 }
 
 float 	elapsed_time ()
 {
-  
+  if(end == 0)
+  {
+    stop_stopwatch();
+  }
+  return float(start - end) / CLOCKS_PER_SEC;
 }
